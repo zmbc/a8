29c2d775fd37a363ead660c88bac43668be047e3
Zeb Burke-Conte <zmbc@uw.edu>
2016-02-25 00:03:27 -0800

    Initial commit

---
+++ server.R
@@ -0,0 +1,96 @@
+library(plotly)
+library(dplyr)
+
+# This function dynamically creates a filter input
+filterInput <- function(filter_num) {
+  filter <- list(
+    selectInput(
+      paste0('filter_', as.character(filter_num), '_column'),
+      label = "Column to filter",
+      choices = colnames(iris)
+    ),
+    selectInput(
+      paste0('filter_', as.character(filter_num), '_comparison'),
+      label = "",
+      choices = list(">", "<", ">=", "<=", "> or <", "=")
+    ),
+    textInput(
+      paste0('filter_', as.character(filter_num), '_value'),
+      label = ""
+    )
+  )
+  return(filter)
+}
+
+shinyServer(function(input, output) {
+  # This creates a list of all the filter inputs needed
+  output$filters <- renderUI({
+    if(input$num_filters < 1) {
+      return()
+    }
+    
+    list_of_filters <- list()
+    
+    for(i in 1:input$num_filters) {
+      list_of_filters[[i]] <- filterInput(i)
+    }
+    
+    return(list_of_filters)
+  })
+  
+  output$scatter <- renderPlotly({
+    filtered_data <- iris
+    # Use the filters to narrow down data
+    for(i in 1:input$num_filters) {
+      filter_name <- paste0('filter_', as.character(i))
+      filter_value <- input[[paste0(filter_name, '_value')]]
+      if(!is.null(filter_value)) {
+        # The filter is filled out
+        column_name <- input[[paste0(filter_name, '_column')]]
+        comparison <- input[[paste0(filter_name, '_comparison')]]
+        if(class(iris[[column_name]]) == "numeric" & !is.na(as.numeric(filter_value))) {
+          # This is a numeric filter
+          if(!grepl(">", comparison)) {
+            # This comparison does not include greater-than
+            filtered_data <- filtered_data %>% filter_(paste(column_name, "<=", filter_value))
+          }
+          
+          if(!grepl("=", comparison)) {
+            # This comparison does not include equals
+            filtered_data <- filtered_data %>% filter_(paste(column_name, "!=", filter_value))
+          }
+          
+          if(!grepl("<", comparison)) {
+            # This comparison does not include less-than
+            filtered_data <- filtered_data %>% filter_(paste(column_name, ">=", filter_value))
+          }
+        } else if(class(iris[[column_name]]) == "character" | class(iris[[column_name]]) == "factor") {
+          # This is a string filter (only works with "=")
+          if(comparison == "=" & nchar(filter_value) > 0) {
+            # Only actually perform the filter if it leaves some values
+            after_filter <- filtered_data %>% filter_(paste0(column_name, " == '", filter_value, "'"))
+            if(nrow(after_filter) > 0) {
+              filtered_data <- after_filter
+            }
+          }
+        }
+      }
+    }
+    
+    # Workaround. Can't find a way to add a title to a color axis
+    # and this was better than having eval AND parse in the auto-title
+    color_axis <- parse(text = input$color_variable)
+    
+    plot_ly(
+      data = filtered_data,
+      x = eval(parse(text = input$x_variable)),
+      y = eval(parse(text = input$y_variable)),
+      color = eval(color_axis),
+      mode = "markers",
+      type = "scatter"
+    ) %>% layout(
+      xaxis = list(title = input$x_variable),
+      yaxis = list(title = input$y_variable)
+    )
+  })
+})
---
+++ ui.R
@@ -0,0 +1,16 @@
+shinyUI(fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      h3("Filters"),
+      numericInput('num_filters', label = "Number of Filters", value = 0),
+      uiOutput('filters'),
+      h3("Variables"),
+      selectInput("x_variable", label = "X Variable", choices = colnames(iris)),
+      selectInput("y_variable", label = "Y Variable", choices = colnames(iris)),
+      selectInput("color_variable", label = "Color Variable", choices = colnames(iris))
+    ),
+    mainPanel(
+      plotlyOutput('scatter')
+    )
+  )
+))
