72f2ff02d1ddb7bc28a27d3d025f798f3a6f726b
Zeb Burke-Conte <zmbc@uw.edu>
2016-02-25 07:45:02 -0800

    Set better defaults

--- ui.R
+++ ui.R
@@ -7,9 +7,9 @@ shinyUI(fluidPage(
       numericInput('num_filters', label = "Number of Filters", value = 0),
       uiOutput('filters'),
       h3("Variables"),
-      selectInput("x_variable", label = "X Variable", choices = colnames(iris)),
-      selectInput("y_variable", label = "Y Variable", choices = colnames(iris)),
-      selectInput("color_variable", label = "Color Variable", choices = colnames(iris))
+      selectInput("x_variable", label = "X Variable", choices = colnames(iris), selected = colnames(iris)[1]),
+      selectInput("y_variable", label = "Y Variable", choices = colnames(iris), selected = colnames(iris)[2]),
+      selectInput("color_variable", label = "Color Variable", choices = colnames(iris), selected = colnames(iris)[5])
     ),
     mainPanel(
       plotlyOutput('scatter')
